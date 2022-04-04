43b6ed66d4273864c533a480d6b7496c56f08849
Francisflow <francisngigi948@gmail.com>
2022-04-05 00:11:11 +0300

    Created a view function for home to render index.html

--- insta/views.py
+++ insta/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from . models import Image
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url='/accounts/login')
+# /@login_required(login_url='/accounts/login')
 def home(request):
-    return render(request, 'index.html')
+    images= Image.get_images().order_by('-date_posted')
+    return render(request, 'index.html', {'images': images})
 
 def welcome(request):
     return HttpResponse("Welcome to instagram")
