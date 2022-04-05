8abae80309652cf5245794db3a9b846d0470ffc8
Francisflow <francisngigi948@gmail.com>
2022-04-06 00:23:18 +0300

    Updated user and newprofile forms

--- insta/forms.py
+++ insta/forms.py
@@ -4,30 +4,36 @@ from django.contrib.auth.models import User
 from .models import Image, Profile, Comment
 
 
-class CommentForm(forms.ModleForm):
+class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['content'].widget=forms.TextInput()
         self.fields['content'].widget.attrs['placeholder']='Comment on this post'
 
     class Meta:
-        modle=Comment
+        model=Comment
         fields=('content',)
 
-class NewProfileForm(forms.ModleForm):
+class NewProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
         exclude=['user']
 
-class UpdateUserForm(forms.ModleForm):
+class UpdateUserForm(forms.ModelForm):
     email=forms.EmailField(max_length=300, help_text="Requried, email address")
+    username=forms.CharField(max_length=40)
     class Meta:
         model=User
         fields=('username', 'email')
 
 
-class UpdateUserProfileForm(forms.ModleForm):
+class UpdateUserProfileForm(forms.ModelForm):
     class Meta:
         model=Profile
-        fields= ['name', 'profile_photo', 'bio']
+        fields= ['name', 'profile_pic', 'bio']
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model=Image
+        exclude=['posted_by', 'date_posted', 'image_likes', 'user']
         
