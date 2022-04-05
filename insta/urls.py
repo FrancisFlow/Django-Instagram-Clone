31c41c60805db28187f1ec39322d1067b8210510
Francisflow <francisngigi948@gmail.com>
2022-04-06 00:18:43 +0300

    Defined routes for creating a new profile and comment

--- insta/urls.py
+++ insta/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('welcome', views.welcome, name='welcome'),
+    path('profile/<profile_id>/', views.profile, name='profile'),
+    path('post/<id>/', views.post_comment, name='comment'),
+    path('new/profile/', views.new_profile, name='new-profile'),
 ]
